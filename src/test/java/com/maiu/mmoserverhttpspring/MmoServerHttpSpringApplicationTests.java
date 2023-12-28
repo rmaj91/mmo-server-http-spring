cf0b6178de602438daf4da3a5affce8fa1141845
rmaj91 <rmaj91@gmail.com>
2023-12-28 19:11:46 +0100

    init

---
+++ src/test/java/com/maiu/mmoserverhttpspring/MmoServerHttpSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiu.mmoserverhttpspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmoServerHttpSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
