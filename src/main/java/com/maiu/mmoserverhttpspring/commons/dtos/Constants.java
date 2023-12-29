3099df7339f195aa5c3ccde9e0c7028422636a20
rmaj91 <rmaj91@gmail.com>
2023-12-29 18:48:22 +0100

    username regexp lowercase

--- src/main/java/com/maiu/mmoserverhttpspring/commons/dtos/Constants.java
+++ src/main/java/com/maiu/mmoserverhttpspring/commons/dtos/Constants.java
@@ -1,6 +1,6 @@
 package com.maiu.mmoserverhttpspring.commons.dtos;
 
 public interface Constants {
-    String USERNAME_VALIDATION_REGEXP = "[a-zA-Z0-9]{4,20}";
+    String USERNAME_VALIDATION_REGEXP = "[a-z0-9]{4,20}";
     String PASSWORD_VALIDATION_REGEXP = "[A-Za-z\\d@$!%*?&]{4,20}";
 }
