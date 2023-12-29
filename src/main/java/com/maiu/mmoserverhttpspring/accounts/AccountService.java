e3bebbae50739ce136af32ec92b2b61bfcc052a8
rmaj91 <rmaj91@gmail.com>
2023-12-29 18:04:01 +0100

    logging account creation

--- src/main/java/com/maiu/mmoserverhttpspring/accounts/AccountService.java
+++ src/main/java/com/maiu/mmoserverhttpspring/accounts/AccountService.java
@@ -3,11 +3,13 @@ package com.maiu.mmoserverhttpspring.accounts;
 import com.maiu.mmoserverhttpspring.commons.dtos.accounts.AccountCreateRequest;
 import com.maiu.mmoserverhttpspring.commons.dtos.accounts.AccountCreatedResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AccountService {
@@ -17,6 +19,7 @@ public class AccountService {
 
     public AccountCreatedResponse createAccount(AccountCreateRequest request) {
         if (accountsRepository.existsByUsername(request.getUsername())) {
+            log.error("Account: '{}' already exists", request.getUsername());
             throw new ResponseStatusException(HttpStatus.CONFLICT);
         }
 
@@ -24,6 +27,7 @@ public class AccountService {
         entity.setUsername(request.getUsername().toLowerCase());//todo remove if test json
         entity.setPasswordHash(passwordEncoder.encode(request.getPassword()));
         AccountEntity saved = accountsRepository.save(entity);
+        log.error("Account: '{}' successfully exists", request.getUsername());
 
         return new AccountCreatedResponse(saved.getUsername());
     }
