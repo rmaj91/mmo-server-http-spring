f3bdfbd1ac2f18da78c51524dab0412494263f94
rmaj91 <rmaj91@gmail.com>
2023-12-30 16:37:29 +0100

    add maxage to cors config

--- src/main/java/com/maiu/mmoserverhttpspring/config/CorsConfig.java
+++ src/main/java/com/maiu/mmoserverhttpspring/config/CorsConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.time.Duration;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
@@ -13,6 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
+                .maxAge(Duration.ofDays(1).toSeconds())
                 .allowCredentials(true);
     }
 }
